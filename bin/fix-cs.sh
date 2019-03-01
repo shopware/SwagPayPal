59ed99e1ca10f5d4154f0ab58bd0bdcb8e841ada
Michael Telgmann <mt@shopware.com>
2019-03-01 16:30:00 +0100

    NTR - Add javscript cs fixer

--- bin/fix-cs.sh
+++ bin/fix-cs.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
+echo "Fix php files"
 ./../../../vendor/shopware/platform/bin/php-cs-fixer.phar fix --config=../../../vendor/shopware/platform/.php_cs.dist -vv .
+
+echo "Fix javascript files"
+../../../vendor/shopware/platform/src/Administration/Resources/administration/node_modules/.bin/eslint --ignore-path .eslintignore --config ../../../vendor/shopware/platform/src/Administration/Resources/administration/.eslintrc.js --ext .js,.vue --fix .
