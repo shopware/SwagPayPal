db018b3f705f675d4e726478118284e25f5bf84e
Michael Telgmann <mt@shopware.com>
2019-03-06 08:43:42 +0100

    NTR - Fix flag import

--- Setting/SwagPayPalSettingGeneralDefinition.php
+++ Setting/SwagPayPalSettingGeneralDefinition.php
@@ -11,12 +11,12 @@ namespace SwagPayPal\Setting;
 use Shopware\Core\Framework\DataAbstractionLayer\EntityDefinition;
 use Shopware\Core\Framework\DataAbstractionLayer\Field\BoolField;
 use Shopware\Core\Framework\DataAbstractionLayer\Field\CreatedAtField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\Flag\PrimaryKey;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\Flag\Required;
 use Shopware\Core\Framework\DataAbstractionLayer\Field\IdField;
 use Shopware\Core\Framework\DataAbstractionLayer\Field\StringField;
 use Shopware\Core\Framework\DataAbstractionLayer\Field\UpdatedAtField;
 use Shopware\Core\Framework\DataAbstractionLayer\FieldCollection;
-use Shopware\Core\Framework\DataAbstractionLayer\Write\Flag\PrimaryKey;
-use Shopware\Core\Framework\DataAbstractionLayer\Write\Flag\Required;
 
 class SwagPayPalSettingGeneralDefinition extends EntityDefinition
 {
