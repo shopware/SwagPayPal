f24cab214ce270d69a7ccaee5ab70f5f4d5374d7
Michael Telgmann <mt@shopware.com>
2019-04-11 15:17:42 +0200

    NTR - Adjust unit test execution

--- bin/phpunit.sh
+++ bin/phpunit.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
+
+composer dump-autoload
 ./../../../vendor/bin/phpunit
---
+++ tests/TestBootstrap.php
@@ -0,0 +1,35 @@
+<?php declare(strict_types=1);
+
+function getProjectDir(): string
+{
+    if (isset($_SERVER['PROJECT_ROOT']) && file_exists($_SERVER['PROJECT_ROOT'])) {
+        return $_SERVER['PROJECT_ROOT'];
+    }
+    if (isset($_ENV['PROJECT_ROOT']) && file_exists($_ENV['PROJECT_ROOT'])) {
+        return $_ENV['PROJECT_ROOT'];
+    }
+
+    $dir = $rootDir = __DIR__;
+    while (!file_exists($dir . '/.env')) {
+        if ($dir === \dirname($dir)) {
+            return $rootDir;
+        }
+        $dir = \dirname($dir);
+    }
+
+    return $dir;
+}
+
+define('TEST_PROJECT_DIR', getProjectDir());
+
+require_once TEST_PROJECT_DIR . '/vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Symfony\Component\Dotenv\Dotenv;
+
+if (!class_exists(Dotenv::class)) {
+    throw new \RuntimeException('APP_ENV environment variable is not defined. You need to define environment variables for configuration or add "symfony/dotenv" as a Composer dependency to load variables from a .env file.');
+}
+(new Dotenv())->load(TEST_PROJECT_DIR . '/.env');
+
+putenv('DATABASE_URL=' . getenv('DATABASE_URL') . '_test');
