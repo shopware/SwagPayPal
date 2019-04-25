f0c698e5af3e4512e743ca1c4b2aaec9d5277ee0
Sebastian Seggewiß <s.seggewiss@shopware.com>
2019-04-25 10:36:11 +0200

    NTR - Change locales to bcp-47 standard

--- src/Resources/views/administration/src/module/swag-paypal/index.js
+++ src/Resources/views/administration/src/module/swag-paypal/index.js
@@ -1,8 +1,8 @@
 import { Module } from 'src/core/shopware';
 import paypalSettings from './page/swag-paypal';
 import './extension/sw-settings-index';
-import deDE from './snippet/de_DE.json';
-import enGB from './snippet/en_GB.json';
+import deDE from './snippet/de-DE.json';
+import enGB from './snippet/en-GB.json';
 
 Module.register('swag-paypal', {
     type: 'plugin',
