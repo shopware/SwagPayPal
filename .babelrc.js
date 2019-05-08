df9548fbfa5818a0e1bd2014fdee7040e9db72b8
Sebastian Seggewiß <s.seggewiss@shopware.com>
2019-05-08 16:00:59 +0200

    NTR - Add static property support to babel

--- .babelrc.js
+++ .babelrc.js
@@ -1,3 +1,7 @@
 module.exports = {
-    presets: ['@babel/preset-env']
+    presets: ['@babel/preset-env'],
+    plugins: [
+        '@babel/plugin-proposal-class-properties',
+        '@babel/plugin-transform-object-assign',
+    ]
 };
