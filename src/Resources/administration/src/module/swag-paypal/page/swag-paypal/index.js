a8cff42984474ceaf8def849ce131e0055f1263f
Sebastian <s.seggewiss@shopware.com>
2019-09-19 14:19:12 +0200

    PT-10705 - Move configuration back to own module

--- src/Resources/administration/src/module/swag-paypal/page/swag-paypal/index.js
+++ src/Resources/administration/src/module/swag-paypal/page/swag-paypal/index.js
@@ -1,4 +1,5 @@
 import template from './swag-paypal.html.twig';
+import './swag-paypal.scss';
 import constants from './swag-paypal-consts';
 
 const { Mixin } = Shopware;
@@ -18,12 +19,85 @@ export default {
         return {
             isLoading: false,
             isSaveSuccessful: false,
-            config: {},
             clientIdFilled: false,
             clientSecretFilled: false,
-            showValidationErrors: false,
+            intentOptions: [
+                {
+                    id: 'sale',
+                    name: this.$tc('swag-paypal.settingForm.behaviour.intent.sale')
+                },
+                {
+                    id: 'authorize',
+                    name: this.$tc('swag-paypal.settingForm.behaviour.intent.authorize')
+                },
+                {
+                    id: 'order',
+                    name: this.$tc('swag-paypal.settingForm.behaviour.intent.order')
+                }
+            ],
+            landingPageOptions: [
+                {
+                    id: 'Login',
+                    name: this.$tc('swag-paypal.settingForm.behaviour.landingPage.options.Login')
+                },
+                {
+                    id: 'Billing',
+                    name: this.$tc('swag-paypal.settingForm.behaviour.landingPage.options.Billing')
+                }
+            ],
+            buttonColorOptions: [
+                {
+                    id: 'blue',
+                    name: this.$tc('swag-paypal.settingForm.express.ecsButtonColor.options.blue')
+                },
+                {
+                    id: 'black',
+                    name: this.$tc('swag-paypal.settingForm.express.ecsButtonColor.options.black')
+                },
+                {
+                    id: 'gold',
+                    name: this.$tc('swag-paypal.settingForm.express.ecsButtonColor.options.gold')
+                },
+                {
+                    id: 'silver',
+                    name: this.$tc('swag-paypal.settingForm.express.ecsButtonColor.options.silver')
+                }
+            ],
+            buttonShapeOptions: [
+                {
+                    id: 'pill',
+                    name: this.$tc('swag-paypal.settingForm.express.ecsButtonShape.options.pill')
+                },
+                {
+                    id: 'rect',
+                    name: this.$tc('swag-paypal.settingForm.express.ecsButtonShape.options.rect')
+                }
+            ],
+            config: null,
+            clientIdErrorState: null,
+            clientSecretErrorState: null,
             ...constants
         };
+    },
+
+    computed: {
+        clientIdError: {
+            get() {
+                return this.clientIdErrorState;
+            },
+            set(value) {
+                this.clientIdErrorState = value;
+            }
+        },
+
+        clientSecretError: {
+            get() {
+                return this.clientSecretErrorState;
+            },
+            set(value) {
+                this.clientSecretErrorState = value;
+            }
+        }
     },
 
     metaInfo() {
@@ -32,95 +106,139 @@ export default {
         };
     },
 
+    watch: {
+        config: {
+            handler() {
+                const defaultConfig = this.$refs.configComponent.allConfigs.null;
+                const salesChannelId = this.$refs.configComponent.selectedSalesChannelId;
+
+                if (salesChannelId === null) {
+                    this.clientIdFilled = !!this.config['SwagPayPal.settings.clientId'];
+                    this.clientSecretFilled = !!this.config['SwagPayPal.settings.clientSecret'];
+                } else {
+                    this.clientIdFilled = !!this.config['SwagPayPal.settings.clientId']
+                        || !!defaultConfig['SwagPayPal.settings.clientId'];
+                    this.clientSecretFilled = !!this.config['SwagPayPal.settings.clientSecret']
+                        || !!defaultConfig['SwagPayPal.settings.clientSecret'];
+                }
+            },
+            deep: true
+        }
+    },
+
     methods: {
-        saveFinish() {
-            this.isSaveSuccessful = false;
-        },
-
-        onConfigChange(config) {
-            this.config = config;
-
-            this.checkCredentialsFilled();
-
-            this.showValidationErrors = false;
-        },
-
-        checkCredentialsFilled() {
-            const defaultConfig = this.$refs.systemConfig.actualConfigData.null;
-            const salesChannelId = this.$refs.systemConfig.currentSalesChannelId;
-
-            if (salesChannelId === null) {
-                this.clientIdFilled = !!this.config['SwagPayPal.settings.clientId'];
-                this.clientSecretFilled = !!this.config['SwagPayPal.settings.clientSecret'];
-            } else {
-                this.clientIdFilled = !!this.config['SwagPayPal.settings.clientId']
-                    || !!defaultConfig['SwagPayPal.settings.clientId'];
-                this.clientSecretFilled = !!this.config['SwagPayPal.settings.clientSecret']
-                    || !!defaultConfig['SwagPayPal.settings.clientSecret'];
+        checkTextFieldInheritance(value) {
+            if (typeof value !== 'string') {
+                return true;
             }
+
+            return value.length <= 0;
+        },
+
+        checkBoolFieldInheritance(value) {
+            if (typeof value !== 'boolean') {
+                return true;
+            }
+
+            return false;
+        },
+
+        getInheritTextValue(key) {
+            const salesChannelId = this.$refs.configComponent.selectedSalesChannelId;
+            const res = salesChannelId == null ? null : this.$refs.configComponent.allConfigs.null[key];
+
+            if (salesChannelId !== null && (typeof res === 'undefined' || res === null)) {
+                return '';
+            }
+
+            return res;
         },
 
         onSave() {
             if (!this.clientIdFilled || !this.clientSecretFilled) {
-                this.showValidationErrors = true;
+                this.setErrorStates();
                 return;
             }
 
-            this.isSaveSuccessful = false;
+            this._save();
+        },
+
+        _save() {
             this.isLoading = true;
-            this.$refs.systemConfig.saveAll().then(() => {
+
+            this.$refs.configComponent.save().then((res) => {
                 this.isLoading = false;
                 this.isSaveSuccessful = true;
 
-                this.SwagPayPalWebhookRegisterService.registerWebhook(this.$refs.systemConfig.currentSalesChannelId)
-                    .then((response) => {
-                        const result = response.result;
+                if (res) {
+                    this.config = res;
+                }
 
-                        if (result === this.WEBHOOK_RESULT_NOTHING) {
-                            return;
-                        }
+                this.createNotificationSuccess({
+                    title: this.$tc('swag-paypal.settingForm.titleSuccess'),
+                    message: this.$tc('swag-paypal.settingForm.messageSaveSuccess')
+                });
 
-                        if (result === this.WEBHOOK_RESULT_CREATED) {
-                            this.createNotificationSuccess({
-                                title: this.$tc('swag-paypal.settingForm.titleSuccess'),
-                                message: this.$tc('swag-paypal.settingForm.messageWebhookCreated')
-                            });
-
-                            return;
-                        }
-
-                        if (result === this.WEBHOOK_RESULT_UPDATED) {
-                            this.createNotificationSuccess({
-                                title: this.$tc('swag-paypal.settingForm.titleSuccess'),
-                                message: this.$tc('swag-paypal.settingForm.messageWebhookUpdated')
-                            });
-                        }
-                        this.isLoading = false;
-                    }).catch((errorResponse) => {
-                        if (errorResponse.response.data && errorResponse.response.data.errors) {
-                            let message = `${this.$tc('swag-paypal.settingForm.messageWebhookError')}<br><br><ul>`;
-                            errorResponse.response.data.errors.forEach((error) => {
-                                message = `${message}<li>${error.detail}</li>`;
-                            });
-                            message += '</li>';
-                            this.createNotificationError({
-                                title: this.$tc('swag-paypal.settingForm.titleError'),
-                                message: message
-                            });
-                        }
-                        this.isLoading = false;
-                    });
+                this.registerWebhook();
             }).catch(() => {
                 this.isLoading = false;
             });
         },
 
+        registerWebhook() {
+            this.SwagPayPalWebhookRegisterService.registerWebhook(this.$refs.configComponent.selectedSalesChannelId)
+                .then((response) => {
+                    const result = response.result;
+
+                    if (result === this.WEBHOOK_RESULT_NOTHING) {
+                        return;
+                    }
+
+                    if (result === this.WEBHOOK_RESULT_CREATED) {
+                        this.createNotificationSuccess({
+                            title: this.$tc('swag-paypal.settingForm.titleSuccess'),
+                            message: this.$tc('swag-paypal.settingForm.messageWebhookCreated')
+                        });
+
+                        return;
+                    }
+
+                    if (result === this.WEBHOOK_RESULT_UPDATED) {
+                        this.createNotificationSuccess({
+                            title: this.$tc('swag-paypal.settingForm.titleSuccess'),
+                            message: this.$tc('swag-paypal.settingForm.messageWebhookUpdated')
+                        });
+                    }
+                    this.isLoading = false;
+                }).catch((errorResponse) => {
+                    if (errorResponse.response.data && errorResponse.response.data.errors) {
+                        let message = `${this.$tc('swag-paypal.settingForm.messageWebhookError')}<br><br><ul>`;
+                        errorResponse.response.data.errors.forEach((error) => {
+                            message = `${message}<li>${error.detail}</li>`;
+                        });
+                        message += '</li>';
+                        this.createNotificationError({
+                            title: this.$tc('swag-paypal.settingForm.titleError'),
+                            message: message
+                        });
+                    }
+                    this.isLoading = false;
+                });
+        },
+
         onTest() {
             this.isLoading = true;
+            const clientId = this.config['SwagPayPal.settings.clientId'] ||
+                this.$refs.configComponent.allConfigs.null['SwagPayPal.settings.clientId'];
+            const clientSecret = this.config['SwagPayPal.settings.clientSecret'] ||
+                this.$refs.configComponent.allConfigs.null['SwagPayPal.settings.clientSecret'];
+            const sandbox = this.config['SwagPayPal.settings.sandbox'] ||
+                this.$refs.configComponent.allConfigs.null['SwagPayPal.settings.sandbox'];
+
             this.SwagPayPalApiCredentialsService.validateApiCredentials(
-                this.config['SwagPayPal.settings.clientId'],
-                this.config['SwagPayPal.settings.clientSecret'],
-                this.config['SwagPayPal.settings.sandbox']
+                clientId,
+                clientSecret,
+                sandbox
             ).then((response) => {
                 const credentialsValid = response.credentialsValid;
 
@@ -147,54 +265,23 @@ export default {
             });
         },
 
-        getBind(element, config) {
-            if (config !== this.config) {
-                this.onConfigChange(config);
-            }
-            if (this.showValidationErrors) {
-                if (element.name === 'SwagPayPal.settings.clientId' && !this.clientIdFilled) {
-                    element.config.error = {
-                        code: 1,
-                        detail: this.$tc('swag-paypal.messageNotBlank')
-                    };
-                }
-                if (element.name === 'SwagPayPal.settings.clientSecret' && !this.clientSecretFilled) {
-                    element.config.error = {
-                        code: 1,
-                        detail: this.$tc('swag-paypal.messageNotBlank')
-                    };
-                }
+        setErrorStates() {
+            if (!this.clientIdFilled) {
+                this.clientIdError = {
+                    code: 1,
+                    detail: this.$tc('swag-paypal.messageNotBlank')
+                };
+            } else {
+                this.clientIdError = null;
             }
 
-            if (element.name === 'SwagPayPal.settings.orderNumberPrefix') {
-                element.config.disabled = !this.config['SwagPayPal.settings.sendOrderNumber'];
-            }
-
-            if (element.name === 'SwagPayPal.settings.plusOverwritePaymentName') {
-                this.setPlusDefaultValue(element, 'plusOverwritePaymentName');
-            }
-
-            if (element.name === 'SwagPayPal.settings.plusExtendPaymentDescription') {
-                this.setPlusDefaultValue(element, 'plusExtendPaymentDescription');
-            }
-
-            if (element.name === 'SwagPayPal.settings.spbButtonLanguageIso') {
-                element.config.disabled = !this.config['SwagPayPal.settings.spbCheckoutEnabled'];
-            }
-
-            if (element.name === 'SwagPayPal.settings.spbAlternativePaymentMethodsEnabled') {
-                element.config.disabled = !this.config['SwagPayPal.settings.spbCheckoutEnabled'];
-            }
-
-            return element;
-        },
-
-        setPlusDefaultValue(element, configName) {
-            element.config.disabled = !this.config['SwagPayPal.settings.plusEnabled'];
-            const configNameWithDomain = `SwagPayPal.settings.${configName}`;
-
-            if (this.config[configNameWithDomain] === undefined || this.config[configNameWithDomain] === '') {
-                this.config[configNameWithDomain] = this.$tc(`swag-paypal.settingForm.plus.${configName}`);
+            if (!this.clientSecretFilled) {
+                this.clientSecretError = {
+                    code: 1,
+                    detail: this.$tc('swag-paypal.messageNotBlank')
+                };
+            } else {
+                this.clientSecretError = null;
             }
         }
     }
