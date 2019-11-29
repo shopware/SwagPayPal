93d28bedb3352bb8aeae7ebd3e0ef1b5c1c4bf79
Markus Velt <m.velt@shopware.com>
2019-11-29 15:53:18 +0100

    NTR - update frw extension

--- src/Resources/app/administration/src/init/translation.init.js
+++ src/Resources/app/administration/src/init/translation.init.js
@@ -1,5 +1,5 @@
-import deDeSnippets from '../app/snippets/de-DE.json';
-import enGBSnippets from '../app/snippets/en-GB.json';
+import deDeSnippets from '../module/extension/sw-first-run-wizard/snippets/de-DE.json';
+import enGBSnippets from '../module/extension/sw-first-run-wizard/snippets/en-GB.json';
 
 Shopware.Locale.extend('de-DE', deDeSnippets);
 Shopware.Locale.extend('en-GB', enGBSnippets);
