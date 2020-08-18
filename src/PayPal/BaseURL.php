7413fde1f3906841efe667989d2c5fe89871bd3f
Michael Telgmann <mt@shopware.com>
2020-08-18 14:19:00 +0200

    NTR - Make BaseURL class final

--- src/PayPal/BaseURL.php
+++ src/PayPal/BaseURL.php
@@ -7,8 +7,15 @@
 
 namespace Swag\PayPal\PayPal;
 
-class BaseURL
+/**
+ * @deprecated tag:v2.0.0 - This class will be final in version 2.0.0
+ */
+/*final */class BaseURL
 {
     public const SANDBOX = 'https://api.sandbox.paypal.com/v1/';
     public const LIVE = 'https://api.paypal.com/v1/';
+
+    private function __construct()
+    {
+    }
 }
