aa307b6f40ca41fb4d60f5c697063826bfb480a2
Michael Telgmann <m.telgmann@shopware.com>
2020-08-18 09:48:34 +0200

    Update ticket numbers

--- src/Util/PaymentStatusUtil.php
+++ src/Util/PaymentStatusUtil.php
@@ -84,7 +84,7 @@ class PaymentStatusUtil
             return;
         }
 
-        // TODO PT-11940 - Do transition even if transaction is already partially paid.
+        // TODO PPI-59 - Do transition even if transaction is already partially paid.
         if ($stateMachineState->getTechnicalName() !== OrderTransactionStates::STATE_PARTIALLY_PAID) {
             $this->reopenTransaction($stateMachineState, $transactionId, $context);
             $this->orderTransactionStateHandler->payPartially($transactionId, $context);
@@ -168,7 +168,7 @@ class PaymentStatusUtil
             throw new InvalidTransactionException($transactionId);
         }
 
-        // TODO PT-11940 - Do transition even if transaction is already partially refunded.
+        // TODO PPI-59 - Do transition even if transaction is already partially refunded.
         if ($stateMachineState->getTechnicalName() !== OrderTransactionStates::STATE_PARTIALLY_REFUNDED) {
             $this->orderTransactionStateHandler->refundPartially($transactionId, $context);
         }
