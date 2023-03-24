7d410c0e6e2d162291065ef7ab953bfa5dde5ad3
Max Stegmeyer <m.stegmeyer@shopware.com>
2023-03-24 12:41:33 +0100

    PPI-756 - Fix not instantiable Service

---
+++ tests/ServiceDefinitionTest.php
@@ -0,0 +1,49 @@
+<?php declare(strict_types=1);
+/*
+ * (c) shopware AG <info@shopware.com>
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Swag\PayPal\Test;
+
+use PHPUnit\Framework\TestCase;
+use Shopware\Core\Framework\Test\TestCaseBase\KernelLifecycleManager;
+use Shopware\Core\Framework\Test\TestCaseBase\KernelTestBehaviour;
+use Shopware\Core\Framework\Test\TestKernel;
+use Symfony\Bundle\FrameworkBundle\Test\TestContainer;
+
+/**
+ * @internal
+ */
+class ServiceDefinitionTest extends TestCase
+{
+    use KernelTestBehaviour;
+
+    public function testEverythingIsInstantiatable(): void
+    {
+        $separateKernel = KernelLifecycleManager::createKernel(
+            TestKernel::class,
+            true,
+            'h8f3f0ee9c61829627676afd6294bb029',
+            $this->getKernel()->getProjectDir()
+        );
+        $separateKernel->boot();
+
+        // @phpstan-ignore-next-line
+        $testContainer = $separateKernel->getContainer()->get('test.service_container');
+
+        static::assertInstanceOf(TestContainer::class, $testContainer);
+
+        $errors = [];
+        foreach ($testContainer->getServiceIds() as $serviceId) {
+            try {
+                $testContainer->get($serviceId);
+            } catch (\Throwable $t) {
+                $errors[] = $serviceId . ':' . $t->getMessage();
+            }
+        }
+
+        static::assertCount(0, $errors, 'Found invalid services: ' . \print_r($errors, true));
+    }
+}
