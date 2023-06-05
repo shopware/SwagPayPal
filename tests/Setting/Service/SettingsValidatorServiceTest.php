d26a6e8c30d69e8d9a63b6682069476557aaf784
Max Stegmeyer <m.stegmeyer@shopware.com>
2023-06-05 23:01:21 +0200

    PPI-769 - Add ECS pay later setting to admin

--- tests/Setting/Service/SettingsValidatorServiceTest.php
+++ tests/Setting/Service/SettingsValidatorServiceTest.php
@@ -33,8 +33,7 @@ class SettingsValidatorServiceTest extends TestCase
         $validationService = new SettingsValidationService($systemSettings, new NullLogger());
         $validationService->validate();
         $validationService->validate(TestDefaults::SALES_CHANNEL);
-        // @phpstan-ignore-next-line
-        static::assertTrue(true);
+        static::expectNotToPerformAssertions();
     }
 
     public function testValidateWithValidLiveDistinctSettings(): void
@@ -61,8 +60,7 @@ class SettingsValidatorServiceTest extends TestCase
         $validationService = new SettingsValidationService($systemSettings, new NullLogger());
         $validationService->validate();
         $validationService->validate(TestDefaults::SALES_CHANNEL);
-        // @phpstan-ignore-next-line
-        static::assertTrue(true);
+        static::expectNotToPerformAssertions();
     }
 
     public function testValidateWithValidSandboxSettingsButSandboxDisabled(): void
