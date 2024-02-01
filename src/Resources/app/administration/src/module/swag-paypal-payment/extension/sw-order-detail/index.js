c3af6b5bfb3f0bde35633c85e431330fa7b9dfd2
Michel Bade <m.bade@shopware.com>
2024-02-01 10:31:40 +0100

    PPI-846 - Changed error for unavailable payment details

--- src/Resources/app/administration/src/module/swag-paypal-payment/extension/sw-order-detail/index.js
+++ src/Resources/app/administration/src/module/swag-paypal-payment/extension/sw-order-detail/index.js
@@ -1,52 +1,23 @@
 import template from './sw-order-detail.html.twig';
 
-const { Component, Context } = Shopware;
-const { hasOwnProperty } = Shopware.Utils.object;
-const Criteria = Shopware.Data.Criteria;
+const { Component } = Shopware;
+const { isEmpty } = Shopware.Utils.types;
 
 Component.override('sw-order-detail', {
     template,
 
-    data() {
-        return {
-            isPayPalPayment: false,
-        };
-    },
-
     computed: {
+        isPayPalPayment() {
+            const transaction = this.order?.transactions?.last();
+
+            return this.order
+                && !isEmpty(transaction)
+                && (transaction.customFields.swag_paypal_order_id
+                    || transaction.customFields.swag_paypal_transaction_id);
+        },
+
         isEditable() {
             return !this.isPayPalPayment || this.$route.name !== 'swag.paypal.payment.detail';
-        },
-    },
-
-    watch: {
-        orderId: {
-            deep: true,
-            handler() {
-                if (!this.orderId) {
-                    return;
-                }
-
-                const orderRepository = this.repositoryFactory.create('order');
-                const orderCriteria = new Criteria(1, 1);
-                orderCriteria.addAssociation('transactions');
-                orderCriteria
-                    .getAssociation('transactions')
-                    .addSorting(Criteria.sort('createdAt', 'DESC'))
-                    .setLimit(1);
-
-                orderRepository.get(this.orderId, Context.api, orderCriteria).then((order) => {
-                    const transaction = order.transactions.last();
-                    if (!transaction) {
-                        return;
-                    }
-
-                    this.isPayPalPayment = hasOwnProperty(transaction, 'customFields') &&
-                        (hasOwnProperty(transaction.customFields, 'swag_paypal_order_id') ||
-                         hasOwnProperty(transaction.customFields, 'swag_paypal_transaction_id'));
-                });
-            },
-            immediate: true,
         },
     },
 });
