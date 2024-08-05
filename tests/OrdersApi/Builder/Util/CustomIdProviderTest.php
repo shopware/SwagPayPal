b00eb7812a8ab3bedc0e99fd8bb42b71c91dad05
Max Stegmeyer <m.stegmeyer@shopware.com>
2024-08-05 10:42:57 +0200

    PPI-958 - Add commercial to custom id

--- tests/OrdersApi/Builder/Util/CustomIdProviderTest.php
+++ tests/OrdersApi/Builder/Util/CustomIdProviderTest.php
@@ -8,6 +8,7 @@
 namespace Swag\PayPal\Test\OrdersApi\Builder\Util;
 
 use PHPUnit\Framework\TestCase;
+use Shopware\Commercial\SwagCommercial;
 use Shopware\Core\Checkout\Order\Aggregate\OrderTransaction\OrderTransactionEntity;
 use Shopware\Core\Checkout\Payment\PaymentMethodDefinition;
 use Shopware\Core\Framework\Context;
@@ -19,6 +20,7 @@ use Shopware\Core\Framework\Plugin\PluginCollection;
 use Shopware\Core\Framework\Plugin\PluginEntity;
 use Shopware\Core\Framework\Uuid\Uuid;
 use Swag\PayPal\OrdersApi\Builder\Util\CustomIdProvider;
+use Swag\PayPal\SwagPayPal;
 
 /**
  * @internal
@@ -32,6 +34,7 @@ class CustomIdProviderTest extends TestCase
         $plugin = new PluginEntity();
         $plugin->setId(Uuid::randomHex());
         $plugin->setVersion('7.1.0');
+        $plugin->setBaseClass(SwagPayPal::class);
         $pluginRepository->expects(static::once())
             ->method('search')
             ->willReturn(new EntitySearchResult(
@@ -50,6 +53,73 @@ class CustomIdProviderTest extends TestCase
         $expected = \json_encode([
             'orderTransactionId' => $transaction->getId(),
             'pluginVersion' => '7.1.0',
+            'shopwareVersion' => '6.5.0.0',
+        ]) ?: '[]';
+
+        static::assertJsonStringEqualsJsonString($expected, $result);
+    }
+
+    public function testCustomIdWithCommercial(): void
+    {
+        $pluginRepository = $this->createMock(EntityRepository::class);
+        $plugin = new PluginEntity();
+        $plugin->setId(Uuid::randomHex());
+        $plugin->setVersion('9.4.0');
+        $plugin->setBaseClass(SwagPayPal::class);
+        $commercial = new PluginEntity();
+        $commercial->setId(Uuid::randomHex());
+        $commercial->setVersion('6.6.0');
+        $commercial->setBaseClass(SwagCommercial::class);
+        $commercial->setActive(true);
+        $pluginRepository->expects(static::once())
+            ->method('search')
+            ->willReturn(new EntitySearchResult(
+                PaymentMethodDefinition::ENTITY_NAME,
+                2,
+                new PluginCollection([$plugin, $commercial]),
+                null,
+                new Criteria(),
+                Context::createDefaultContext(),
+            ));
+        $provider = new CustomIdProvider($pluginRepository, '6.5.0.0');
+        $transaction = new OrderTransactionEntity();
+        $transaction->setId(Uuid::randomHex());
+
+        $result = $provider->createCustomId($transaction, Context::createDefaultContext());
+        $expected = \json_encode([
+            'orderTransactionId' => $transaction->getId(),
+            'pluginVersion' => '9.4.0-c',
+            'shopwareVersion' => '6.5.0.0',
+        ]) ?: '[]';
+
+        static::assertJsonStringEqualsJsonString($expected, $result);
+    }
+
+    public function testCustomIdWithInvalidVersion(): void
+    {
+        $pluginRepository = $this->createMock(EntityRepository::class);
+        $plugin = new PluginEntity();
+        $plugin->setId(Uuid::randomHex());
+        $plugin->setVersion('this-may-be-a-custom-branch-name');
+        $plugin->setBaseClass(SwagPayPal::class);
+        $pluginRepository->expects(static::once())
+            ->method('search')
+            ->willReturn(new EntitySearchResult(
+                PaymentMethodDefinition::ENTITY_NAME,
+                1,
+                new PluginCollection([$plugin]),
+                null,
+                new Criteria(),
+                Context::createDefaultContext(),
+            ));
+        $provider = new CustomIdProvider($pluginRepository, '6.5.0.0');
+        $transaction = new OrderTransactionEntity();
+        $transaction->setId(Uuid::randomHex());
+
+        $result = $provider->createCustomId($transaction, Context::createDefaultContext());
+        $expected = \json_encode([
+            'orderTransactionId' => $transaction->getId(),
+            'pluginVersion' => '0.0.0',
             'shopwareVersion' => '6.5.0.0',
         ]) ?: '[]';
 
