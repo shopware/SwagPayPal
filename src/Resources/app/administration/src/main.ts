7e5ec07a1958fbbb2a4adf821e67263137a21e8a
Michel Bade <m.bade@shopware.com>
2024-10-14 10:15:20 +0200

    PPI-1001 - Removed admin html entrypoint

--- src/Resources/app/administration/src/main.ts
+++ src/Resources/app/administration/src/main.ts
@@ -1,28 +1,43 @@
-import { location } from '@shopware-ag/meteor-admin-sdk';
+import { ui } from '@shopware-ag/meteor-admin-sdk';
 
-const bootPromise = window.Shopware
-    ? (Shopware.Plugin.addBootPromise() as () => never) // it's wrongly typed as object in shopware
-    : undefined;
+import './mixin/swag-paypal-credentials-loader.mixin';
+import './mixin/swag-paypal-notification.mixin';
+import './mixin/swag-paypal-pos-catch-error.mixin';
+import './mixin/swag-paypal-pos-log-label.mixin';
 
-(async () => {
-    if (!location.isIframe()) {
-        await import('./mixin/swag-paypal-credentials-loader.mixin');
-        await import('./mixin/swag-paypal-notification.mixin');
-        await import('./mixin/swag-paypal-pos-catch-error.mixin');
-        await import('./mixin/swag-paypal-pos-log-label.mixin');
+import './module/extension';
+import './module/swag-paypal';
+import './module/swag-paypal-disputes';
+import './module/swag-paypal-payment';
+import './module/swag-paypal-pos';
 
-        await import('./module/extension');
-        await import('./module/swag-paypal');
-        await import('./module/swag-paypal-disputes');
-        await import('./module/swag-paypal-payment');
-        await import('./module/swag-paypal-pos');
+import './init/api-service.init';
+import './init/translation.init';
+import './init/svg-icons.init';
 
-        await import('./init/api-service.init');
-        await import('./init/translation.init');
-        await import('./init/svg-icons.init');
-    }
-
-    if (bootPromise) {
-        bootPromise();
-    }
-})();
+ui.module.payment.overviewCard.add({
+    positionId: 'swag-paypal-overview-card-before',
+    component: 'swag-paypal-overview-card',
+    paymentMethodHandlers: [
+        'handler_swag_trustlyapmhandler',
+        'handler_swag_sofortapmhandler',
+        'handler_swag_p24apmhandler',
+        'handler_swag_oxxoapmhandler',
+        'handler_swag_mybankapmhandler',
+        'handler_swag_multibancoapmhandler',
+        'handler_swag_idealapmhandler',
+        'handler_swag_giropayapmhandler',
+        'handler_swag_epsapmhandler',
+        'handler_swag_blikapmhandler',
+        'handler_swag_bancontactapmhandler',
+        'handler_swag_sepahandler',
+        'handler_swag_acdchandler',
+        'handler_swag_puihandler',
+        'handler_swag_paypalpaymenthandler',
+        'handler_swag_pospayment',
+        'handler_swag_venmohandler',
+        'handler_swag_paylaterhandler',
+        'handler_swag_applepayhandler',
+        'handler_swag_googlepayhandler',
+    ],
+});
