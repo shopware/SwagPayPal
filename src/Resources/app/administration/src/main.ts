f88a74f5d80be4035e5600e053267ef13f5bdae2
Michel Bade <m.bade@shopware.com>
2024-11-18 09:53:23 +0100

    PPI-1021 - Fixed subdirectory asset loading

--- src/Resources/app/administration/src/main.ts
+++ src/Resources/app/administration/src/main.ts
@@ -1,3 +1,5 @@
+import { ui } from '@shopware-ag/meteor-admin-sdk';
+
 import './mixin/swag-paypal-credentials-loader.mixin';
 import './mixin/swag-paypal-notification.mixin';
 import './mixin/swag-paypal-pos-catch-error.mixin';
@@ -12,3 +14,30 @@ import './module/swag-paypal-pos';
 import './init/api-service.init';
 import './init/translation.init';
 import './init/svg-icons.init';
+
+ui.module.payment.overviewCard.add({
+    positionId: 'swag-paypal-overview-card-before',
+    component: 'swag-paypal-overview-card',
+    paymentMethodHandlers: [
+        'handler_swag_trustlyapmhandler',
+        'handler_swag_sofortapmhandler',
+        'handler_swag_p24apmhandler',
+        'handler_swag_oxxoapmhandler',
+        'handler_swag_mybankapmhandler',
+        'handler_swag_multibancoapmhandler',
+        'handler_swag_idealapmhandler',
+        'handler_swag_giropayapmhandler',
+        'handler_swag_epsapmhandler',
+        'handler_swag_blikapmhandler',
+        'handler_swag_bancontactapmhandler',
+        'handler_swag_sepahandler',
+        'handler_swag_acdchandler',
+        'handler_swag_puihandler',
+        'handler_swag_paypalpaymenthandler',
+        'handler_swag_pospayment',
+        'handler_swag_venmohandler',
+        'handler_swag_paylaterhandler',
+        'handler_swag_applepayhandler',
+        'handler_swag_googlepayhandler',
+    ],
+});
