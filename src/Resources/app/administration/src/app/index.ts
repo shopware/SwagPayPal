0fa5f4de8cea8b54e9c779c1501a263c9be4c5b0
Michel Bade <m.bade@shopware.com>
2024-12-01 16:06:46 +0100

    PPI-1037 - Added swag-paypal-onboarding-button component

--- src/Resources/app/administration/src/app/index.ts
+++ src/Resources/app/administration/src/app/index.ts
@@ -1,5 +1,7 @@
 import './store/swag-paypal-merchant-information.store';
 import './store/swag-paypal-settings.store';
+
+Shopware.Component.register('swag-paypal-onboarding-button', () => import('./component/swag-paypal-onboarding-button'));
 Shopware.Component.register('swag-paypal-setting', () => import('./component/swag-paypal-setting'));
 
 // synchronise salesChannel of stores
