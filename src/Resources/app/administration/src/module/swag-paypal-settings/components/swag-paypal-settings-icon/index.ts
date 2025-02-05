6c100216014b378f960c9e9f28ed133d6f821e86
Michel Bade <m.bade@shopware.com>
2025-02-05 16:05:13 +0100

    fix: lint vite type complain

--- src/Resources/app/administration/src/module/swag-paypal-settings/components/swag-paypal-settings-icon/index.ts
+++ src/Resources/app/administration/src/module/swag-paypal-settings/components/swag-paypal-settings-icon/index.ts
@@ -1,11 +1,12 @@
 import template from './swag-paypal-settings-icon.html.twig';
 import './swag-paypal-settings-icon.scss';
-import IconsPaypalMulticolor from '../../../../app/assets/icons/svg/icons-paypal-multicolor.svg?component';
+import IconsPaypalMulticolor from 'SwagPayPal/app/assets/icons/svg/icons-paypal-multicolor.svg?component';
 
 export default Shopware.Component.wrapComponentConfig({
     template,
 
     components: {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
         IconsPaypalMulticolor,
     },
 
