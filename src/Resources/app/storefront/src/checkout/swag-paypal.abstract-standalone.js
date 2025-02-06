37a7583756775337bf5ee3d90ecb4d96e7bda95c
Michel Bade <m.bade@shopware.com>
2025-02-06 13:03:29 +0100

    fix: order form submit with disabled button

--- src/Resources/app/storefront/src/checkout/swag-paypal.abstract-standalone.js
+++ src/Resources/app/storefront/src/checkout/swag-paypal.abstract-standalone.js
@@ -199,7 +199,7 @@ export default class SwagPaypalAbstractStandalone extends SwagPaypalAbstractButt
         input.setAttribute('value', data.orderID ?? data.orderId);
 
         this.confirmOrderForm.appendChild(input);
-        DomAccess.querySelector(this.confirmOrderForm, this.options.confirmOrderButtonSelector).click();
+        this.confirmOrderForm.submit();
     }
 
     /**
