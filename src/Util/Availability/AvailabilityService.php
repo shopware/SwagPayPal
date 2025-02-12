d9b689f1efa8dbe2a52007aa17b3542d982d9a4d
Albert Scherman <a.scherman@shopware.com>
2025-02-12 12:37:04 +0100

    fix: hopefully last review change

--- src/Util/Availability/AvailabilityService.php
+++ src/Util/Availability/AvailabilityService.php
@@ -11,9 +11,7 @@ use Shopware\Core\Checkout\Cart\Cart;
 use Shopware\Core\Checkout\Order\OrderEntity;
 use Shopware\Core\Checkout\Payment\PaymentMethodCollection;
 use Shopware\Core\Checkout\Payment\PaymentMethodEntity;
-use Shopware\Core\Content\Product\State;
 use Shopware\Core\Framework\Log\Package;
-use Shopware\Core\Framework\Struct\ArrayStruct;
 use Shopware\Core\System\SalesChannel\SalesChannelContext;
 use Swag\PayPal\Util\Lifecycle\Method\PaymentMethodDataRegistry;
 
@@ -55,12 +53,7 @@ class AvailabilityService
     {
         $handlers = [];
 
-        $context = AvailabilityContextBuilder::buildFromCart($cart, $salesChannelContext);
-        $context->assign([
-            'totalAmount' => $order->getPrice()->getTotalPrice(),
-            'subscription' => $order->getExtensionOfType('foreignKeys', ArrayStruct::class)?->get('subscriptionId') !== null,
-            'hasDigitalProducts' => (bool) $order->getLineItems()?->hasLineItemWithState(State::IS_DOWNLOAD),
-        ]);
+        $context = AvailabilityContextBuilder::buildFromOrder($order, $salesChannelContext);
 
         foreach ($paymentMethods as $paymentMethod) {
             if (!$this->isAvailable($paymentMethod, $context)) {
