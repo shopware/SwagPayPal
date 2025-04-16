9a091595977ea763e61dade7538fd7d6bf69b011
Michel Bade <m.bade@shopware.com>
2025-04-16 17:13:44 +0200

    fix: revert changes to order route

--- src/Checkout/SalesChannel/CreateOrderRoute.php
+++ src/Checkout/SalesChannel/CreateOrderRoute.php
@@ -22,7 +22,6 @@ use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Sorting\FieldSorting;
 use Shopware\Core\Framework\Log\Package;
 use Shopware\Core\Framework\Plugin\Exception\DecorationPatternException;
-use Shopware\Core\Framework\ShopwareHttpException;
 use Shopware\Core\Framework\Validation\DataBag\RequestDataBag;
 use Shopware\Core\System\SalesChannel\SalesChannelContext;
 use Shopware\PayPalSDK\Struct\V2\Order;
@@ -118,54 +117,40 @@ class CreateOrderRoute extends AbstractCreateOrderRoute
                 default => $this->payPalOrderBuilder,
             };
 
-            [$paypalOrder, $requestId] = $orderId
+            $paypalOrder = $orderId
                 ? $this->getOrderFromOrder($orderBuilder, $orderId, $customer, $request, $salesChannelContext)
                 : $this->getOrderFromCart($orderBuilder, $salesChannelContext, $request);
 
             $salesChannelId = $salesChannelContext->getSalesChannelId();
-            $response = $this->orderResource->create(
-                $paypalOrder,
-                $salesChannelId,
-                $this->getPartnerAttributionId($product),
-                // requestId: $requestId,
-            );
+            $response = $this->orderResource->create($paypalOrder, $salesChannelId, $this->getPartnerAttributionId($product));
 
             return new TokenResponse($response->getId());
-        } catch (ShopwareHttpException $e) {
-            $this->logger->error($e->getMessage(), ['error' => \iterator_to_array($e->getErrors())]);
+        } catch (\Throwable $e) {
+            $this->logger->error($e->getMessage(), ['error' => $e]);
 
             throw $e;
         }
     }
 
-    /**
-     * @return array{0: Order, 1: null}
-     */
     private function getOrderFromCart(
         AbstractOrderBuilder $orderBuilder,
         SalesChannelContext $salesChannelContext,
         Request $request,
-    ): array {
+    ): Order {
         $cart = $this->cartService->getCart($salesChannelContext->getToken(), $salesChannelContext);
 
         $requestDataBag = new RequestDataBag($request->request->all());
 
-        return [
-            $orderBuilder->getOrderFromCart($cart, $salesChannelContext, $requestDataBag),
-            null,
-        ];
+        return $orderBuilder->getOrderFromCart($cart, $salesChannelContext, $requestDataBag);
     }
 
-    /**
-     * @return array{0: Order, 1: string}
-     */
     private function getOrderFromOrder(
         AbstractOrderBuilder $orderBuilder,
         string $orderId,
         CustomerEntity $customer,
         Request $request,
         SalesChannelContext $salesChannelContext,
-    ): array {
+    ): Order {
         $criteria = new Criteria([$orderId]);
         $criteria->addAssociation('transactions');
         $criteria->addAssociation('lineItems');
@@ -198,16 +183,13 @@ class CreateOrderRoute extends AbstractCreateOrderRoute
             throw PaymentException::invalidOrder($orderId);
         }
 
-        return [
-            $orderBuilder->getOrder(
-                $this->paymentTransactionStructFactory->build($transaction->getPaymentMethodId(), $salesChannelContext->getContext()),
-                $transaction,
-                $order,
-                $salesChannelContext->getContext(),
-                $request,
-            ),
-            $transaction->getId() . ($transaction->getUpdatedAt()?->getTimestamp() ?: ''),
-        ];
+        return $orderBuilder->getOrder(
+            $this->paymentTransactionStructFactory->build($transaction->getPaymentMethodId(), $salesChannelContext->getContext()),
+            $transaction,
+            $order,
+            $salesChannelContext->getContext(),
+            $request,
+        );
     }
 
     private function getPartnerAttributionId(string $product): string
