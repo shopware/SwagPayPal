6954d93d9fc9e135bb16729102c8c9a70a082f18
Michel Bade <m.bade@shopware.com>
2025-07-16 13:02:33 +0200

    fix: openapi schema generation

--- src/DevOps/Command/GenerateOpenApi.php
+++ src/DevOps/Command/GenerateOpenApi.php
@@ -78,7 +78,10 @@ class GenerateOpenApi extends Command
         $openApi = $generator->generate([
             Util::finder(self::ROOT_DIR . '/src/RestApi'),
             Util::finder(self::ROOT_DIR . '/src/Checkout'),
-            Util::finder(self::ROOT_DIR . '/../../../vendor/shopware/paypal-sdk/src/Struct'),
+            Util::finder(
+                self::ROOT_DIR . '/../../../vendor/shopware/paypal-sdk/src/Struct',
+                [self::ROOT_DIR . '/../../../vendor/shopware/paypal-sdk/src/Struct/AgenticCommerce'],
+            ),
         ])?->toJson();
 
         if ($openApi === null) {
@@ -110,7 +113,10 @@ class GenerateOpenApi extends Command
             Util::finder(self::ROOT_DIR . '/src/Pos'),
             Util::finder(self::ROOT_DIR . '/src/Setting'),
             Util::finder(self::ROOT_DIR . '/src/Webhook'),
-            Util::finder(self::ROOT_DIR . '/../../../vendor/shopware/paypal-sdk/src/Struct'),
+            Util::finder(
+                self::ROOT_DIR . '/../../../vendor/shopware/paypal-sdk/src/Struct',
+                [self::ROOT_DIR . '/../../../vendor/shopware/paypal-sdk/src/Struct/AgenticCommerce'],
+            ),
             Util::finder(__DIR__ . '/Polyfill'),
         ])?->toJson();
 
