598013b4e0511e8844ef4ed8f9699ac5189545f3
Max Stegmeyer <m.stegmeyer@shopware.com>
2025-08-05 13:34:33 +0200

    fix: method exists

--- src/Checkout/PUI/PUISubscriber.php
+++ src/Checkout/PUI/PUISubscriber.php
@@ -109,7 +109,7 @@ class PUISubscriber implements EventSubscriberInterface
 
         // @deprecated tag:v11.0.0 - remove early return with min-version of 6.7.2.0
         // @phpstan-ignore-next-line method may or may not exist depending on Shopware version
-        if (method_exists($event->getPage(), 'setLogoutCustomer')) {
+        if (!method_exists($event->getPage(), 'setLogoutCustomer')) {
             return;
         }
 
