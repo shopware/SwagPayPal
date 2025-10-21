72772a3b7fec62f6f7c8d48246d4818a4d1cb2f7
Dang Ng <markng1993@gmail.com>
2025-10-21 11:41:34 +0700

    Revert RequestService.php

--- src/RestApi/RequestService.php
+++ src/RestApi/RequestService.php
@@ -7,10 +7,8 @@
 
 namespace Swag\PayPal\RestApi;
 
-use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
 use Shopware\Core\Framework\Log\Package;
-use Shopware\PayPalSDK\Contract\Context\ApiContextInterface;
 use Shopware\PayPalSDK\Contract\RequestServiceInterface;
 use Shopware\PayPalSDK\Exception\ApiException;
 use Shopware\PayPalSDK\RequestService as SDKRequestService;
@@ -20,30 +18,15 @@ use Swag\PayPal\RestApi\Exception\PayPalApiException;
  * Wrap ApiExceptions into PayPalApiExceptions
  */
 #[Package('checkout')]
-class RequestService implements RequestServiceInterface
+class RequestService extends SDKRequestService implements RequestServiceInterface
 {
-    public function __construct(
-        private readonly SDKRequestService $requestService
-    ) {
-    }
-
-    public function createRequest(string $method, string $path, ApiContextInterface $context): RequestInterface
-    {
-        return $this->requestService->createRequest($method, $path, $context);
-    }
-
-    public function withBody(RequestInterface $request, array|\JsonSerializable $body): RequestInterface
-    {
-        return $this->requestService->withBody($request, $body);
-    }
-
     /**
      * @throws PayPalApiException
      */
     public function handleResponse(ResponseInterface $response): ?array
     {
         try {
-            return $this->requestService->handleResponse($response);
+            return parent::handleResponse($response);
         } catch (ApiException $e) {
             throw PayPalApiException::from($e);
         }
