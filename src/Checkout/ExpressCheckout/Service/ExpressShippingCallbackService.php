c289ce8e773926cd51dcbd62f768317fd1397919
Michel <m.bade@shopware.com>
2025-10-31 15:29:24 +0100

    fix: express checkout shipping callback (#428)

--- src/Checkout/ExpressCheckout/Service/ExpressShippingCallbackService.php
+++ src/Checkout/ExpressCheckout/Service/ExpressShippingCallbackService.php
@@ -8,134 +8,124 @@
 namespace Swag\PayPal\Checkout\ExpressCheckout\Service;
 
 use Psr\Log\LoggerInterface;
-use Shopware\Core\Checkout\Cart\Price\Struct\CalculatedPrice;
-use Shopware\Core\Checkout\Cart\Price\Struct\CartPrice;
+use Shopware\Core\Checkout\Cart\Cart;
 use Shopware\Core\Checkout\Cart\SalesChannel\CartService;
-use Shopware\Core\Framework\Context;
+use Shopware\Core\Checkout\Shipping\Cart\Error\ShippingMethodBlockedError;
 use Shopware\Core\Framework\DataAbstractionLayer\EntityRepository;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\OrFilter;
 use Shopware\Core\Framework\Log\Package;
 use Shopware\Core\Framework\Validation\DataBag\RequestDataBag;
-use Shopware\Core\System\Country\CountryEntity;
+use Shopware\Core\System\Country\CountryCollection;
 use Shopware\Core\System\SalesChannel\Context\AbstractSalesChannelContextFactory;
 use Shopware\Core\System\SalesChannel\Context\SalesChannelContextService;
 use Shopware\Core\System\SalesChannel\SalesChannel\AbstractContextSwitchRoute;
 use Shopware\Core\System\SalesChannel\SalesChannelContext;
-use Shopware\PayPalSDK\Struct\V2\Order\PurchaseUnit;
-use Swag\PayPal\Checkout\CheckoutException;
-use Swag\PayPal\OrdersApi\Builder\Util\AmountProvider;
+use Shopware\PayPalSDK\Struct\V2\Order;
+use Shopware\PayPalSDK\Struct\V2\OrderShippingCallback;
+use Swag\PayPal\Checkout\ExpressCheckout\ExpressShippingCallbackException;
+use Swag\PayPal\OrdersApi\Builder\AbstractOrderBuilder;
+use Swag\PayPal\OrdersApi\Builder\Util\ShippingOptionsProvider;
 
 #[Package('checkout')]
 class ExpressShippingCallbackService
 {
     /**
      * @internal
+     *
+     * @param EntityRepository<CountryCollection> $countryRepository
      */
     public function __construct(
         private readonly CartService $cartService,
         private readonly EntityRepository $countryRepository,
-        private readonly AmountProvider $amountProvider,
+        private readonly AbstractOrderBuilder $orderBuilder,
+        private readonly ShippingOptionsProvider $shippingOptionsProvider,
         private readonly AbstractContextSwitchRoute $contextSwitchRoute,
         private readonly AbstractSalesChannelContextFactory $salesChannelContextFactory,
         private readonly LoggerInterface $logger,
     ) {
     }
 
-    /**
-     * @param array<string, mixed> $shippingAddress
-     *
-     * @return array<int, array<string, mixed>>
-     */
     public function recalculateCart(
-        string $paypalOrderId,
-        array $shippingAddress,
+        OrderShippingCallback $callback,
         SalesChannelContext $salesChannelContext,
-    ): array {
-        $this->logger->debug('Shipping callback: Recalculating cart for address change', [
-            'paypalOrderId' => $paypalOrderId,
-            'countryCode' => $shippingAddress['country_code'] ?? null,
-            'fullAddress' => $shippingAddress,
-        ]);
+    ): Order {
+        if ($this->hasContextChanges($callback, $salesChannelContext)) {
+            $salesChannelContext = $this->switchSalesChannelContext($callback, $salesChannelContext);
 
-        // Get country by ISO code
-        $countryCode = $shippingAddress['country_code'] ?? null;
-        if (!$countryCode) {
-            $this->logger->error('Shipping callback: Missing country code in shipping address');
-            throw CheckoutException::expressMissingCountryCode();
+            $this->logger->debug('Shipping callback: recalculating cart with new context');
+            $cart = $this->cartService->getCart($salesChannelContext->getToken(), $salesChannelContext, false);
+        } else {
+            $this->logger->debug('Shipping callback: use existing cart');
+            $cart = $this->cartService->getCart($salesChannelContext->getToken(), $salesChannelContext);
         }
 
-        $country = $this->getCountryByIso($countryCode, $salesChannelContext->getContext());
+        $order = $this->orderBuilder->getOrderFromCart($cart, $salesChannelContext, new RequestDataBag());
+        $order->setId($callback->getId());
+        $order->getPurchaseUnits()->first()?->setReferenceId((string) $callback->getPurchaseUnits()->first()?->getReferenceId());
+        $order->getPurchaseUnits()->first()?->setShippingOptions($this->shippingOptionsProvider->getShippingOptions($cart, $salesChannelContext));
+
+        if ((int) $order->getPurchaseUnits()->first()?->getShippingOptions()?->count() === 0) {
+            throw ExpressShippingCallbackException::addressError($callback);
+        }
+
+        if ($error = $cart->getErrors()->filterInstance(ShippingMethodBlockedError::class)->first()) {
+            /** @var ShippingMethodBlockedError $error */
+            throw ExpressShippingCallbackException::methodUnavailable($callback, $error);
+        }
+
+        $this->logger->debug('Shipping callback: update order', ['order' => $order]);
+
+        return $order;
+    }
+
+    private function hasContextChanges(OrderShippingCallback $callback, SalesChannelContext $salesChannelContext): bool
+    {
+        if ($callback->getShippingAddress()->getCountryCode() !== $salesChannelContext->getShippingLocation()->getCountry()->getIso()) {
+            return true;
+        }
+
+        if (($shippingMethodId = $callback->getShippingOption()?->getId()) && $shippingMethodId !== $salesChannelContext->getShippingMethod()->getId()) {
+            return true;
+        }
+
+        if (($state = $salesChannelContext->getShippingLocation()->getState()) && !\in_array($callback->getShippingAddress()->getAdminArea1(), [$state->getTranslation('name'), $state->getShortCode()], true)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private function switchSalesChannelContext(OrderShippingCallback $callback, SalesChannelContext $salesChannelContext): SalesChannelContext
+    {
+        $criteria = new Criteria();
+        $criteria->addFilter(new EqualsFilter('iso', $callback->getShippingAddress()->getCountryCode()));
+        $criteria->setLimit(1);
+        $criteria->getAssociation('states')
+            ->addFilter(new OrFilter([
+                new EqualsFilter('name', $callback->getShippingAddress()->getAdminArea1()),
+                new EqualsFilter('shortCode', $callback->getShippingAddress()->getAdminArea1()),
+            ]))
+            ->setLimit(1);
+
+        $country = $this->countryRepository->search($criteria, $salesChannelContext->getContext())->getEntities()->first();
         if (!$country) {
-            $this->logger->error('Country not found', ['countryCode' => $countryCode]);
-            throw CheckoutException::expressCountryNotFound($countryCode);
+            throw ExpressShippingCallbackException::countryError($callback);
         }
 
         // The current context should not be affected by the shipping country
         // That the customer selects in the PayPal UI
         // Hence a new context for the cart recalculation is created
-        $this->logger->debug('Switching context to new country', ['countryId' => $country->getId()]);
-        $newToken = $this->contextSwitchRoute->switchContext(
-            new RequestDataBag([
-                SalesChannelContextService::COUNTRY_ID => $country->getId(),
-            ]),
-            $salesChannelContext
-        )->getToken();
-
-        $newSalesChannelContext = $this->salesChannelContextFactory->create(
-            $newToken,
-            $salesChannelContext->getSalesChannel()->getId()
-        );
-
-        // Recalculate cart with new context
-        $this->logger->debug('Recalculating cart with new country context');
-        $cart = $this->cartService->recalculate(
-            $this->cartService->getCart($newSalesChannelContext->getToken(), $newSalesChannelContext),
-            $newSalesChannelContext
-        );
-
-        // Build the response in PayPal's expected format
-        $currency = $newSalesChannelContext->getCurrency();
-        $totalPrice = new CalculatedPrice(
-            $cart->getPrice()->getTotalPrice(),
-            $cart->getPrice()->getTotalPrice(),
-            $cart->getPrice()->getCalculatedTaxes(),
-            $cart->getPrice()->getTaxRules()
-        );
-
-        // Create a temporary PurchaseUnit to use AmountProvider
-        $purchaseUnit = new PurchaseUnit();
-        $amount = $this->amountProvider->createAmount(
-            $totalPrice,
-            $cart->getShippingCosts(),
-            $currency,
-            $purchaseUnit,
-            $cart->getPrice()->getTaxStatus() !== CartPrice::TAX_STATE_GROSS
-        );
-        $purchaseUnit->setAmount($amount);
-
-        $purchaseUnitArray = $purchaseUnit->jsonSerialize();
-
-        $this->logger->debug('Cart recalculation completed', [
-            'paypalOrderId' => $paypalOrderId,
-            'newCountry' => $countryCode,
-            'newTotal' => $amount->getValue(),
-            'newTax' => $amount->getBreakdown()?->getTaxTotal()?->getValue(),
-            'newShippingCosts' => $cart->getShippingCosts()->getTotalPrice(),
+        $params = \array_filter([
+            SalesChannelContextService::COUNTRY_ID => $country->getId(),
+            SalesChannelContextService::COUNTRY_STATE_ID => $country->getStates()?->first()?->getId(),
+            SalesChannelContextService::SHIPPING_METHOD_ID => $callback->getShippingOption()?->getId(),
         ]);
 
-        return [$purchaseUnitArray];
-    }
+        $this->logger->debug('Shipping callback: switching context to new country', ['context_parameters' => $params]);
+        $token = $this->contextSwitchRoute->switchContext(new RequestDataBag($params), $salesChannelContext)->getToken();
 
-    private function getCountryByIso(string $iso, Context $context): ?CountryEntity
-    {
-        $criteria = new Criteria();
-        $criteria->addFilter(new EqualsFilter('iso', $iso));
-        $criteria->setLimit(1);
-
-        /** @var CountryEntity|null $country */
-        $country = $this->countryRepository->search($criteria, $context)->first();
-
-        return $country;
+        return $this->salesChannelContextFactory->create($token, $salesChannelContext->getSalesChannelId(), $params);
     }
 }
