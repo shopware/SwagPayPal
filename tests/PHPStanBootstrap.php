bf78160c2ec171aba3addf5a9203b837f685f017
Michel Bade <m.bade@shopware.com>
2025-11-06 15:28:38 +0100

    fix: plugin path

--- tests/PHPStanBootstrap.php
+++ tests/PHPStanBootstrap.php
@@ -18,9 +18,17 @@ if (!\class_exists(TestBootstrapper::class)) {
 
 $bootstrapper = (new TestBootstrapper())->setProjectDir($projectRoot);
 
+$getPluginPath = function (string $name) use ($projectRoot): ?string {
+    if (\is_readable($projectRoot . '/custom/plugins/' . $name . '/composer.json')) {
+        return $projectRoot . '/custom/plugins/' . $name;
+    }
+
+    return null;
+};
+
 $plugins = ['SwagPayPal'];
 foreach (['SwagCmsExtensions', 'SwagCommercial'] as $pluginName) {
-    if ($bootstrapper->getPluginPath($pluginName)) {
+    if ($getPluginPath($pluginName)) {
         $plugins[] = $pluginName;
 
         echo "{$pluginName} detected, require being active." . \PHP_EOL;
@@ -32,9 +40,9 @@ $bootstrapper = $bootstrapper
     ->addActivePlugins(...$plugins);
 
 $pluginLoader = new StaticKernelPluginLoader($bootstrapper->getClassLoader(), plugins: \array_map(
-    function (string $plugin) use ($bootstrapper) {
+    function (string $plugin) use ($getPluginPath) {
         /** @var array{autoload: array{}, version: string, extra: array{}} $composer */
-        $composer = \json_decode(\file_get_contents($bootstrapper->getPluginPath($plugin) . '/composer.json') ?: '', true, flags: \JSON_THROW_ON_ERROR);
+        $composer = \json_decode(\file_get_contents($getPluginPath($plugin) . '/composer.json') ?: '', true, flags: \JSON_THROW_ON_ERROR);
 
         return [
             'name' => $plugin,
@@ -43,7 +51,7 @@ $pluginLoader = new StaticKernelPluginLoader($bootstrapper->getClassLoader(), pl
             'baseClass' => $composer['extra']['shopware-plugin-class'] ?? '',
             'managedByComposer' => false, // even though some are, namespaces wouldn't load if set true
             'autoload' => $composer['autoload'],
-            'path' => $bootstrapper->getPluginPath($plugin),
+            'path' => $getPluginPath($plugin),
         ];
     },
     $plugins,
