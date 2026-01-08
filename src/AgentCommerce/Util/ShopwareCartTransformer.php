41c5583a26a5dd67e1bf74c1b3646e4f2dbef07c
Fabian Boensch <f.boensch@shopware.com>
2026-01-08 08:46:48 +0100

    use sales channel repository

--- src/AgentCommerce/Util/ShopwareCartTransformer.php
+++ src/AgentCommerce/Util/ShopwareCartTransformer.php
@@ -11,7 +11,6 @@ use Shopware\Core\Checkout\Cart\LineItem\LineItem;
 use Shopware\Core\Checkout\Cart\LineItemFactoryHandler\ProductLineItemFactory;
 use Shopware\Core\Checkout\Customer\Aggregate\CustomerGroup\CustomerGroupCollection;
 use Shopware\Core\Checkout\Promotion\Cart\PromotionItemBuilder;
-use Shopware\Core\Framework\Context;
 use Shopware\Core\Framework\DataAbstractionLayer\EntityRepository;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
@@ -20,6 +19,7 @@ use Shopware\Core\Framework\Log\Package;
 use Shopware\Core\Framework\Uuid\Uuid;
 use Shopware\Core\System\Country\Aggregate\CountryState\CountryStateCollection;
 use Shopware\Core\System\Country\CountryCollection;
+use Shopware\Core\System\SalesChannel\Entity\SalesChannelRepository;
 use Shopware\Core\System\SalesChannel\SalesChannelContext;
 use Shopware\Core\System\Salutation\SalutationCollection;
 use Shopware\Core\System\Salutation\SalutationDefinition;
@@ -40,12 +40,12 @@ use Swag\PayPal\AgentCommerce\Exception\AgentException;
 class ShopwareCartTransformer
 {
     /**
-     * @param EntityRepository<CountryCollection> $countryRepository
+     * @param SalesChannelRepository<CountryCollection> $countryRepository
      * @param EntityRepository<SalutationCollection> $salutationRepository
      * @param EntityRepository<CustomerGroupCollection> $groupRepository
      */
     public function __construct(
-        private readonly EntityRepository $countryRepository,
+        private readonly SalesChannelRepository $countryRepository,
         private readonly EntityRepository $salutationRepository,
         private readonly EntityRepository $groupRepository,
         private readonly ProductLineItemFactory $lineItemFactory,
@@ -56,7 +56,7 @@ class ShopwareCartTransformer
     /**
      * @return array{firstName: string, lastName: string, email: string|null, salesChannelId: string, groupId: string|null, shippingAddress: array, guest: true, billingAddress?: array}
      */
-    public function extractCustomerData(PayPalCart $cart, string $salesChannelId, Context $context): array
+    public function extractCustomerData(PayPalCart $cart, string $salesChannelId, SalesChannelContext $context): array
     {
         /** @var Customer $customer */
         $customer = $cart->getCustomer();
@@ -71,7 +71,7 @@ class ShopwareCartTransformer
             'lastName' => $customer->getName()->getSurname(),
             'email' => $customer->getEmailAddress(),
             'salesChannelId' => $salesChannelId,
-            'groupId' => $this->groupRepository->searchIds($groupCriteria, $context)->firstId(),
+            'groupId' => $this->groupRepository->searchIds($groupCriteria, $context->getContext())->firstId(),
             'shippingAddress' => $this->formatAddress($shippingAddress, $customer->getName(), $customer->getPhone(), $context),
             'guest' => true,
         ];
@@ -100,7 +100,7 @@ class ShopwareCartTransformer
         return $lineItems;
     }
 
-    private function formatAddress(Address $address, CustomerName $name, ?Phone $phone, Context $context): array
+    private function formatAddress(Address $address, CustomerName $name, ?Phone $phone, SalesChannelContext $context): array
     {
         $criteria = (new Criteria())->addFilter(new EqualsFilter('iso', $address->getCountryCode()));
 
@@ -115,7 +115,7 @@ class ShopwareCartTransformer
         }
 
         $criteria = (new Criteria())->addFilter(new EqualsFilter('salutationKey', SalutationDefinition::NOT_SPECIFIED));
-        $salutationId = $this->salutationRepository->searchIds($criteria, $context)->firstId();
+        $salutationId = $this->salutationRepository->searchIds($criteria, $context->getContext())->firstId();
 
         return [
             'id' => Uuid::randomHex(),
