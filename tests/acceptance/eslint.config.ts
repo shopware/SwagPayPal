93b03980989076b82647e70f7a557cbec903c4a2
Michel Bade <m.bade@shopware.com>
2026-03-09 17:14:55 +0100

    fix: typechecking

--- tests/acceptance/eslint.config.ts
+++ tests/acceptance/eslint.config.ts
@@ -3,8 +3,9 @@ import eslint from '@eslint/js';
 import playwright from 'eslint-plugin-playwright';
 import stylistic from '@stylistic/eslint-plugin';
 import globals from 'globals';
+import { defineConfig } from 'eslint/config';
 
-export default tseslint.config(
+export default defineConfig(
     eslint.configs.recommended,
     tseslint.configs.recommendedTypeChecked,
     playwright.configs['flat/recommended'],
