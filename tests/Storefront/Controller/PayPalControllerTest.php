7834b2c5b67a0fe4a0d96f95414aa24923c07b65
Max Stegmeyer <m.stegmeyer@shopware.com>
2026-03-30 13:51:57 +0200

    fix: phpstan (#595)

--- tests/Storefront/Controller/PayPalControllerTest.php
+++ tests/Storefront/Controller/PayPalControllerTest.php
@@ -35,8 +35,8 @@ use Symfony\Component\HttpFoundation\Session\Storage\MockArraySessionStorage;
 /**
  * @internal
  */
-#[CoversClass(PayPalController::class)]
 #[Package('checkout')]
+#[CoversClass(PayPalController::class)]
 class PayPalControllerTest extends TestCase
 {
     private AbstractCreateOrderRoute&MockObject $createOrderRoute;
