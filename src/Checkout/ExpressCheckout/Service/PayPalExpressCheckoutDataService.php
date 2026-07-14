3de0958fb94a3bd9a57c51f5f9e8ac34f674f055
Ali Ghanei <88658555+Aliaaaam@users.noreply.github.com>
2026-07-14 12:43:12 +0200

    fix: block Express Checkout on empty cart (#736)
    (cherry picked from commit 1d50fabc3c7a7903b6dec26be1799ab7ff440b2f)

--- src/Checkout/ExpressCheckout/Service/PayPalExpressCheckoutDataService.php
+++ src/Checkout/ExpressCheckout/Service/PayPalExpressCheckoutDataService.php
@@ -58,11 +58,7 @@ class PayPalExpressCheckoutDataService extends AbstractScriptDataService impleme
     ): ?ExpressCheckoutButtonData {
         $cart = $this->cartService->getCart($salesChannelContext->getToken(), $salesChannelContext);
 
-        if (!$addProductToCart && $cart->getLineItems()->count() === 0) {
-            return null;
-        }
-
-        if (!$addProductToCart && $this->cartPriceService->hasZeroPrice($cart, $salesChannelContext)) {
+        if (!$addProductToCart && !$this->cartPriceService->isProcessable($cart, $salesChannelContext)) {
             return null;
         }
 
