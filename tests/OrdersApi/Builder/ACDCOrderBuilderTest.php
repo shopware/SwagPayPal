76b1e62896f033d7c309d76b59b8e1ab06056653
Vladimir Kalchenko <vova@websolutionsnyc.com>
2026-07-23 14:58:24 +0200

    feat: use SCA_WHEN_REQUIRED for ACDC when 3DS is not enforced (#717)
    Co-authored-by: Ali Ghanei <88658555+Aliaaaam@users.noreply.github.com>
    Co-authored-by: Chuc Le <c.lemanh@shopware.com>
    Co-authored-by: Max Stegmeyer <m.stegmeyer@shopware.com>
    (cherry picked from commit 4bd28dce50514a713a3e4fedf0664cdf956828d5)

--- tests/OrdersApi/Builder/ACDCOrderBuilderTest.php
+++ tests/OrdersApi/Builder/ACDCOrderBuilderTest.php
@@ -7,11 +7,15 @@
 
 namespace Swag\PayPal\Test\OrdersApi\Builder;
 
+use Shopware\Core\Checkout\Payment\Cart\SyncPaymentTransactionStruct;
 use Shopware\Core\Framework\Log\Package;
+use Shopware\Core\Framework\Validation\DataBag\RequestDataBag;
 use Swag\PayPal\OrdersApi\Builder\AbstractOrderBuilder;
 use Swag\PayPal\OrdersApi\Builder\ACDCOrderBuilder;
 use Swag\PayPal\OrdersApi\Builder\Util\AddressProvider;
 use Swag\PayPal\RestApi\V2\Api\Order\PaymentSource\Card;
+use Swag\PayPal\RestApi\V2\Api\Order\PaymentSource\Common\Attributes\Verification;
+use Swag\PayPal\Setting\Settings;
 use Swag\PayPal\Test\OrdersApi\Builder\Trait\VaultableOrderBuildTrait;
 
 /**
@@ -21,6 +25,38 @@ use Swag\PayPal\Test\OrdersApi\Builder\Trait\VaultableOrderBuildTrait;
 class ACDCOrderBuilderTest extends AbstractOrderBuilderTestCase
 {
     use VaultableOrderBuildTrait;
+
+    public function testGetOrderUsesScaWhenRequiredByDefault(): void
+    {
+        $paymentTransaction = new SyncPaymentTransactionStruct($this->createOrderTransaction(), $this->createOrder());
+
+        $order = $this->getBuilder()->getOrder(
+            $paymentTransaction,
+            $this->createSalesChannelContext(),
+            new RequestDataBag(),
+        );
+
+        $card = $order->getPaymentSource()?->first(Card::class);
+        static::assertInstanceOf(Card::class, $card);
+        static::assertSame(Verification::METHOD_SCA_WHEN_REQUIRED, $card->getAttributes()?->getVerification()?->getMethod());
+    }
+
+    public function testGetOrderUsesScaAlwaysIfForce3dsEnabled(): void
+    {
+        $this->systemConfig->set(Settings::ACDC_FORCE_3DS, true);
+
+        $paymentTransaction = new SyncPaymentTransactionStruct($this->createOrderTransaction(), $this->createOrder());
+
+        $order = $this->getBuilder()->getOrder(
+            $paymentTransaction,
+            $this->createSalesChannelContext(),
+            new RequestDataBag(),
+        );
+
+        $card = $order->getPaymentSource()?->first(Card::class);
+        static::assertInstanceOf(Card::class, $card);
+        static::assertSame(Verification::METHOD_SCA_ALWAYS, $card->getAttributes()?->getVerification()?->getMethod());
+    }
 
     protected function getBuilder(): AbstractOrderBuilder
     {
