26a0af9051696b29dc72d5817da63787d49840c8
Jonas Elfering <j.elfering@shopware.com>
2026-09-08 15:23:34 +0200

    chore(phpstan): report unmatched ignored errors

--- bin/phpstan-config-generator.php
+++ bin/phpstan-config-generator.php
@@ -34,7 +34,7 @@ $phpstanConfig = [
     ),
     'parameters' => [
         'symfony' => ['containerXmlPath' => \sprintf('%s/%s%sDebugContainer.xml', $kernel->getCacheDir(), str_replace('\\', '_', $kernel::class), \ucfirst($kernel->getEnvironment()))],
-        'reportUnmatchedIgnoredErrors' => !((bool) $_SERVER['CI']),
+        'reportUnmatchedIgnoredErrors' => true,
     ],
 ];
 
